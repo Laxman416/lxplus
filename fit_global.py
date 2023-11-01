1cb0d699e31ea6410d2edb599110dd467abe67c0
Laxman Seelan <laxman.seelan@student.manchester.uk>
2023-11-01 19:33:06 +0000

    Changed parametrs v4

--- fit_global.py
+++ fit_global.py
@@ -266,6 +266,6 @@ else:
 fitResult.Print()
 
 # Get results
-parameters = np.array([mean.getValV(), sigma.getValV(), Csig.getValV(), aL.getValV(), nL.getValV(), aR.getValV(), nR.getValV(), a0.getValV(), frac_D0_down.getValv(), frac_D0_up.getValv(), frac_D0bar_down.getValv(), frac_D0bar_up.getValv()])
+parameters = np.array([mean.getValV(), sigma.getValV(), Csig.getValV(), aL.getValV(), nL.getValV(), aR.getValV(), nR.getValV(), a0.getValV(), frac_D0_down.getValV(), frac_D0_up.getValV(), frac_D0bar_down.getValV(), frac_D0bar_up.getValV()])
 np.savetxt(f"{args.path}/fit_parameters.txt", parameters, delimiter=',')
 print("My program took", time.time() - start_time, "to run")
