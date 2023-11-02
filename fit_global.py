9b70f6aaa88491fefe2f05aaaa8c01775f650790
sjtaylor-9 <staylor7087@gmail.com>
2023-11-02 22:03:34 +0000

    Changed initial guesses

--- fit_global.py
+++ fit_global.py
@@ -126,10 +126,10 @@ gaussian = RooGaussian("gauss", "gauss", D0_M, mean, sigma)
 
 # Model CrystalBall
 Csig = RooRealVar("Csig", "Csig", 5.59, 0, 20)
-aL = RooRealVar("aL", "aL", 5.16, -10, 10)
+aL = RooRealVar("aL", "aL", 3.94, -10, 10)
 nL = RooRealVar("nL", "nL", 0.39, -10, 10)
-aR = RooRealVar("aR", "aR", -4.53, -10, 10)
-nR = RooRealVar("nR", "nR", 16.53, -20, 20)
+aR = RooRealVar("aR", "aR", -6.73, -10, 10)
+nR = RooRealVar("nR", "nR", 1.5, -20, 20)
 crystal = RooCrystalBall("Crystal", "Crystal Ball", D0_M, mean, Csig, aL, nL, aR, nR)
 
 # Model Exponential Background
@@ -137,7 +137,7 @@ a0 = RooRealVar("a0", "a0", -0.0073, -1, 0)
 background = RooExponential("exponential", "exponential", D0_M, a0)
 
 # Model Signal
-frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.78, 0, 1)
+frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.753, 0, 1)
 frac_D0_down = RooRealVar("frac_D0_down", "frac_D0_down", 0.5, 0.7, 0.75)
 frac_D0bar_up = RooRealVar("frac_D0bar_up", "frac_D0bar_up", 0.5, 0.7, 0.75)
 frac_D0bar_down = RooRealVar("frac_D0bar_down", "frac_D0bar_down", 0.5, 0.7, 0.75)
