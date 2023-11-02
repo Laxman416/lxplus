22ac264b01660c3c4569994f13232aefe3d2ff89
sjtaylor-9 <staylor7087@gmail.com>
2023-11-02 22:26:06 +0000

    Changed initial guesses

--- fit_global.py
+++ fit_global.py
@@ -126,8 +126,8 @@ gaussian = RooGaussian("gauss", "gauss", D0_M, mean, sigma)
 
 # Model CrystalBall
 Csig = RooRealVar("Csig", "Csig", 5.63, 0, 20)
-aL = RooRealVar("aL", "aL", 3.51, -10, 10)
-nL = RooRealVar("nL", "nL", 0.92, 0, 6)
+aL = RooRealVar("aL", "aL", 3.51, 0, 5)
+nL = RooRealVar("nL", "nL", 4, 0, 6)
 aR = RooRealVar("aR", "aR", -6.73, -10, 10)
 nR = RooRealVar("nR", "nR", 4.3, 1, 5)
 crystal = RooCrystalBall("Crystal", "Crystal Ball", D0_M, mean, Csig, aL, nL, aR, nR)
