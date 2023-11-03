014e53b2d326f4004cd759589602061a9c53aa1f
sjtaylor-9 <staylor7087@gmail.com>
2023-11-03 21:29:39 +0000

    Changed intial guesses

--- fit_global.py
+++ fit_global.py
@@ -121,15 +121,15 @@ D0_M = ROOT.RooRealVar("D0_MM", "D0 mass / [MeV/c*c]", 1810, 1910)
 
 # Model Gaussian
 mean = RooRealVar("mean", "mean", 1866, 1860, 1870)
-sigma = RooRealVar("sigma", "sigma", 7.9, 0, 100)
+sigma = RooRealVar("sigma", "sigma", 6.3, 0, 100)
 gaussian = RooGaussian("gauss", "gauss", D0_M, mean, sigma)
 
 # Model CrystalBall
-Csig = RooRealVar("Csig", "Csig", 5.4, 0, 100)
-aL = RooRealVar("aL", "aL", 1.48, -30, 30)
-nL = RooRealVar("nL", "nL", 9, -30, 30)
-aR = RooRealVar("aR", "aR", 6.91, -30, 30)
-nR = RooRealVar("nR", "nR", 3.92, -30, 30)
+Csig = RooRealVar("Csig", "Csig", 9.6, 0, 100)
+aL = RooRealVar("aL", "aL", 1.72, -10, 10)
+nL = RooRealVar("nL", "nL", 16, -40, 40)
+aR = RooRealVar("aR", "aR", 2.3, -10, 10)
+nR = RooRealVar("nR", "nR", 28, -40, 40)
 crystal = RooCrystalBall("Crystal", "Crystal Ball", D0_M, mean, Csig, aL, nL, aR, nR)
 
 # Model Exponential Background
@@ -137,10 +137,10 @@ a0 = RooRealVar("a0", "a0", -0.0072, -1, 0)
 background = RooExponential("exponential", "exponential", D0_M, a0)
 
 # Model Signal
-frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.62, 0, 0.8)
-frac_D0_down = RooRealVar("frac_D0_down", "frac_D0_down", 0.62, 0, 0.8)
-frac_D0bar_up = RooRealVar("frac_D0bar_up", "frac_D0bar_up", 0.62, 0, 0.8)
-frac_D0bar_down = RooRealVar("frac_D0bar_down", "frac_D0bar_down", 0.62, 0, 0.8)
+frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.3, 0, 1)
+frac_D0_down = RooRealVar("frac_D0_down", "frac_D0_down", 0.3, 0, 1)
+frac_D0bar_up = RooRealVar("frac_D0bar_up", "frac_D0bar_up", 0.3, 0, 1)
+frac_D0bar_down = RooRealVar("frac_D0bar_down", "frac_D0bar_down", 0.3, 0, 1)
 
 if binned:
     # Creating the histograms for both polarities for D0 and D0bar by converting the TTree D0_MM data inside the TChain to a TH1(base class of ROOT histograms)
