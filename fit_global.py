d25f2231a29ca9f8a2a641ca37ffb11be028d8ec
sjtaylor-9 <staylor7087@gmail.com>
2023-11-03 22:13:05 +0000

    Changed intial guesses

--- fit_global.py
+++ fit_global.py
@@ -121,15 +121,15 @@ D0_M = ROOT.RooRealVar("D0_MM", "D0 mass / [MeV/c*c]", 1810, 1910)
 
 # Model Gaussian
 mean = RooRealVar("mean", "mean", 1865, 1860, 1870)
-sigma = RooRealVar("sigma", "sigma", 8.62, 5, 15)
+sigma = RooRealVar("sigma", "sigma", 8.63, 5, 15)
 gaussian = RooGaussian("gauss", "gauss", D0_M, mean, sigma)
 
 # Model CrystalBall
-Csig = RooRealVar("Csig", "Csig", 6.22, 5, 15)
-aL = RooRealVar("aL", "aL", 2.04, -10, 10)
-nL = RooRealVar("nL", "nL", 8.27, 8, 10)
-aR = RooRealVar("aR", "aR", 5.59, -10, 10)
-nR = RooRealVar("nR", "nR", 29.9, 20, 50)
+Csig = RooRealVar("Csig", "Csig", 6.23, 5, 15)
+aL = RooRealVar("aL", "aL", 3.59, -10, 10)
+nL = RooRealVar("nL", "nL", 8.29, 8, 10)
+aR = RooRealVar("aR", "aR", 9.0, -10, 10)
+nR = RooRealVar("nR", "nR", 42.3, 20, 50)
 crystal = RooCrystalBall("Crystal", "Crystal Ball", D0_M, mean, Csig, aL, nL, aR, nR)
 
 # Model Exponential Background
