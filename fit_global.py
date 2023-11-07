37b0663f1dc46b1e42afbdd7caf5992dd9d03fe9
sjtaylor-9 <staylor7087@gmail.com>
2023-11-07 15:17:14 +0000

    Changed to 50 bins

--- fit_global.py
+++ fit_global.py
@@ -118,30 +118,30 @@ D0_M = ROOT.RooRealVar("D0_MM", "D0 mass / [MeV/c*c]", 1810, 1910)
 
 # Model Gaussian
 mean = RooRealVar("mean", "mean", 1865, 1860, 1870)
-sigma = RooRealVar("sigma", "sigma", 6.0, 2, 30)
+sigma = RooRealVar("sigma", "sigma", 6.31, 2, 30)
 gaussian = RooGaussian("gauss", "gauss", D0_M, mean, sigma)
 
 # Model CrystalBall
-Csig = RooRealVar("Csig", "Csig", 9.9, 2, 30)
-aL = RooRealVar("aL", "aL", 1.6, -20, 20)
-nL = RooRealVar("nL", "nL", 9.9, -20, 30)
-aR = RooRealVar("aR", "aR", 12, -20, 20)
-nR = RooRealVar("nR", "nR", 15, -20, 30)
+Csig = RooRealVar("Csig", "Csig", 9.73, 2, 30)
+aL = RooRealVar("aL", "aL", 1.66, -20, 20)
+nL = RooRealVar("nL", "nL", 16.4, -20, 30)
+aR = RooRealVar("aR", "aR", 11.08, -20, 20)
+nR = RooRealVar("nR", "nR", 2.79, -20, 30)
 crystal = RooCrystalBall("Crystal", "Crystal Ball", D0_M, mean, Csig, aL, nL, aR, nR)
 
 # Model Exponential Background
-a0 = RooRealVar("a0", "a0", -0.0073, -1, 0)
+a0 = RooRealVar("a0", "a0", -0.0067, -1, 0)
 background = RooExponential("exponential", "exponential", D0_M, a0)
 
 # Ratio of signal intensities between each model. For N PDFs need N-1 fractions 
 # DO MagUp
-frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.6, 0, 1)
+frac_D0_up = RooRealVar("frac_D0_up", "frac_D0_up", 0.504, 0, 1)
 # D0 MagDown
-frac_D0_down = RooRealVar("frac_D0_down", "frac_D0_down", 0.6, 0, 1)
+frac_D0_down = RooRealVar("frac_D0_down", "frac_D0_down", 0.503, 0, 1)
 # D0bar MagUp
-frac_D0bar_up = RooRealVar("frac_D0bar_up", "frac_D0bar_up", 0.6, 0, 1)
+frac_D0bar_up = RooRealVar("frac_D0bar_up", "frac_D0bar_up", 0.509, 0, 1)
 # D0bar MagDown
-frac_D0bar_down = RooRealVar("frac_D0bar_down", "frac_D0bar_down", 0.6, 0, 1)
+frac_D0bar_down = RooRealVar("frac_D0bar_down", "frac_D0bar_down", 0.508, 0, 1)
 
 # Generate normalization variables
 Nsig_D0_up = ROOT.RooRealVar("Nsig_D0_up", "Nsig_D0_up", 0.95*ttree_D0_up.GetEntries(), 0, ttree_D0_up.GetEntries())
